2f14cb3485625597170d64b4ec1e8e28bb62c87d
Yuni-Q <carnotcycle2@naver.com>
2018-09-01 18:43:15 +0900

    end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import styled, { injectGlobal, css } from "styled-components";
-
+import styled, { injectGlobal, ThemeProvider } from "styled-components";
+import theme from "./theme";
 injectGlobal`
   body{
     padding: 0;
@@ -8,35 +8,41 @@ injectGlobal`
   }
 `;
 
-const awsomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0, 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding 20px;
+const Card = styled.div`
+  background-color: red;
 `;
-
-const Input = styled.input.attrs({
-  required: true
-})`
-  border: none;
-  border-radius: 5px;
-  ${awsomeCard};
-`;
-
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Input placeholder="hello" />
-      </Container>
-    );
-  }
-}
 
 const Container = styled.div`
   height: 100vh;
   width 100%;
   background-color: pink;
+  ${Card}{ 
+    background-color: blue;
+  }
 `;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
+    );
+  }
+}
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 export default App;
